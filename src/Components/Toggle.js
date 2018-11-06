584768b9df9a95e0d0e04cae52ac9b9b88ff13ce
Richard <>
2018-11-06 11:47:48 +0800

    Done

--- src/Components/Toggle.js
+++ src/Components/Toggle.js
@@ -4,7 +4,17 @@ class Toggle extends Component {
     state = {  }
     render() { 
         return (  
-            <div>Toggle</div>
+            
+              
+             <div className="switch center">
+                <label>
+                <span className="white-text">Off(Pick from unique)</span>
+                    <input type="checkbox" onChange={this.props.toggled}></input>
+                    <span className="lever"></span>
+                <span className="white-text">On(Pick from all students)</span>
+                </label>
+             </div>
+           
         );
     }
 }
